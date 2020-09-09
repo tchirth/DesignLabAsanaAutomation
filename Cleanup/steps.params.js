591f0f21a273a57c6cc66de591cd9de8a51f7cd3
tchirth <53357195+tchirth@users.noreply.github.com>
2020-09-09 16:32:37 -0700

    Delete steps.params.js

--- Cleanup/steps.params.js
+++
@@ -1,9 +0,0 @@
-async (event, steps, params) => {
-    const datetime = require('node-datetime');
-    //Date and time will be used later for logging
-    dt = datetime.create()
-    this.date = dt.format('Y-m-d')
-    this.start_time = dt.format('H:M:S')
-    this.header = params.header
-    this.gsheet_id = params.google_sheet_id  
-}
